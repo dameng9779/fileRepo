6c4e44768eeec797853b6d4e10bff1d64c94f69f
dameng9779 <63384368+dameng9779@users.noreply.github.com>
2023-10-12 19:22:33 +0800

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 config_file="/root/config_info.txt"
 cd /root/emby/fileRepo
-# 获取外部传入的 X-Emby-Token
-x_emby_token="$1"
-
-if [ -z "$x_emby_token" ]; then
-    echo "X-Emby-Token is missing. Please provide the X-Emby-Token as a parameter when running the script."
-    exit 1
-fi
 
 # 获取本机IP地址
 local_ip=$(curl -4 ip.gs)
 
+emby_access_token=$(curl --silent --request POST --url "http://${local_ip}:8096/emby/Users/authenticatebyname?X-Emby-Client=Emby%20Web&X-Emby-Device-Name=Chrome%20Windows&X-Emby-Device-Id=1&X-Emby-Client-Version=4.7.14.0&X-Emby-Language=zh-cn" --form 'Username="emby"' --form 'Pw="emby"' | jq -r '.AccessToken')
+echo "$emby_access_token"
+
 # 调用第一个接口
-curl --silent --request POST --url "http://${local_ip}:8096/emby/Auth/Keys?App=emby&X-Emby-Token=${x_emby_token}"
+curl --silent --request POST --url "http://${local_ip}:8096/emby/Auth/Keys?App=emby&X-Emby-Token=${emby_access_token}"
 
 # 调用第二个接口，获取响应并提取AccessToken
-response=$(curl --silent --request GET --url "http://${local_ip}:8096/emby/Auth/Keys?X-Emby-Token=${x_emby_token}")
+response=$(curl --silent --request GET --url "http://${local_ip}:8096/emby/Auth/Keys?X-Emby-Token=${emby_access_token}")
 emby_key=$(echo "$response" | jq -r '.Items[] | select(.AppName == "emby") | .AccessToken')
 
 # 输出AccessToken
@@ -39,17 +35,3 @@ sed -i "s|const alistPublicAddr = 'http://youralist.com:5244';|const alistPublic
 #启动nginx
 awk '/version: '\''3.5'\''/,/restart: always/' docker-compose.yml > new_docker_compose_file.yml && mv new_docker_compose_file.yml docker-compose.yml
 docker compose up -d
-
-
-
-
-
-
-
-
-
-
-
-
-
-
