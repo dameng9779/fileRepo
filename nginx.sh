00f73f27052a0b1a239dad5cfe5452b4df60c793
dameng9779 <63384368+dameng9779@users.noreply.github.com>
2023-10-12 14:06:50 +0800

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -13,10 +13,10 @@ fi
 local_ip=$(curl -4 ip.gs)
 
 # 调用第一个接口
-curl --silent --request POST --url "http://${local_ip}/emby/Auth/Keys?App=emby&X-Emby-Token=${x_emby_token}"
+curl --silent --request POST --url "http://${local_ip}:8096/emby/Auth/Keys?App=emby&X-Emby-Token=${x_emby_token}"
 
 # 调用第二个接口，获取响应并提取AccessToken
-response=$(curl --silent --request GET --url "http://${local_ip}/emby/Auth/Keys?X-Emby-Token=${x_emby_token}")
+response=$(curl --silent --request GET --url "http://${local_ip}:8096/emby/Auth/Keys?X-Emby-Token=${x_emby_token}")
 emby_key=$(echo "$response" | jq -r '.Items[] | select(.AppName == "emby") | .AccessToken')
 
 # 输出AccessToken
@@ -39,4 +39,17 @@ sed -i "s|const alistPublicAddr = 'http://youralist.com:5244';|const alistPublic
 #启动nginx
 awk '/version: '\''3.5'\''/,/restart: always/' docker-compose.yml > new_docker_compose_file.yml && mv new_docker_compose_file.yml docker-compose.yml
 docker compose up -d
+
+
+
+
+
+
+
+
+
+
+
+
+
 
