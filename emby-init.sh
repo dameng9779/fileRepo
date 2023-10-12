eab90d78be01caa2bb0e9865c3ddab1ffd81a7b3
dameng9779 <63384368+dameng9779@users.noreply.github.com>
2023-10-12 17:12:23 +0800

    Update emby-init.sh

--- emby-init.sh
+++ emby-init.sh
@@ -7,5 +7,5 @@ curl --silent --request POST --url "http://${local_ip}:8096/emby/Startup/User" -
 curl --silent --request POST --url "http://${local_ip}:8096/emby/Startup/Configuration" --form 'UICulture="zh-CN"'  --form 'MetadataCountryCode="US"' --form 'PreferredMetadataLanguage="en"'
 curl --silent --request POST --url "http://${local_ip}:8096/emby/Startup/RemoteAccess" --form 'EnableAutomaticPortMapping="true"'
 curl --silent --request POST --url "http://${local_ip}:8096/emby/Startup/Complete"
-emby-access-token=$(curl --silent --request POST --url "http://${local_ip}:8096/emby/Users/authenticatebyname?X-Emby-Client=Emby%20Web&X-Emby-Device-Name=Chrome%20Windows&X-Emby-Device-Id=1&X-Emby-Client-Version=4.7.14.0&X-Emby-Language=zh-cn" --form 'Username="emby"' --form 'Pw="emby"' | jq -r '.AccessToken')
-echo "$emby-access-token"
+emby_access_token=$(curl --silent --request POST --url "http://${local_ip}:8096/emby/Users/authenticatebyname?X-Emby-Client=Emby%20Web&X-Emby-Device-Name=Chrome%20Windows&X-Emby-Device-Id=1&X-Emby-Client-Version=4.7.14.0&X-Emby-Language=zh-cn" --form 'Username="emby"' --form 'Pw="emby"' | jq -r '.AccessToken')
+echo "$emby_access_token"
